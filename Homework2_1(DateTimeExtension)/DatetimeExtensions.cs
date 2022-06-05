7d3f117d54884a477a556988b03ae53f2b65fdf6
Melih ÖZASLAN <61635941+MelihOzaslan01@users.noreply.github.com>
2022-06-05 16:52:10 +0300

    Homework2_1(DateTimeExtension)

---
+++ Homework2_1(DateTimeExtension)/DatetimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_1_DateTimeExtension_
+{
+    public static class DatetimeExtensions
+    {
+        public static string Ago(this DateTime time)
+        {
+            var now = DateTime.Now;
+            Console.WriteLine(time );
+
+        }
+    }
+}
